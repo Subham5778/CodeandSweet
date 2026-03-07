a28938fdc51a1d13ff40db57ebd11ed0734d07b5
Subham5778 <167920208+Subham5778@users.noreply.github.com>
2026-03-07 07:44:00 +0530

    Implement addSweet function and improve getAllSweets response

--- backend/controllers/sweetController.js
+++ backend/controllers/sweetController.js
@@ -1,9 +1,29 @@
 import Sweet from "../models/Sweet.js";
 
+// Add Sweet (Upload Image to Cloudinary)
+export const addSweet = async (req, res) => {
+  try {
+    const sweet = new Sweet({
+      name: req.body.name,
+      price: req.body.price,
+      image: req.file ? req.file.path : null
+    });
+
+    const savedSweet = await sweet.save();
+
+    res.status(201).json(savedSweet);
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// Get All Sweets
 export const getAllSweets = async (req, res) => {
   try {
     const sweets = await Sweet.find();
-    res.json(sweets);
+    res.status(200).json(sweets);
+
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
